0595af1e711a0b74b11ef192414086c5c953476d
luojh <623597383@qq.com>
2020-04-28 09:49:34 +0800

    新增文件

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 前端脚手架
 1.react，react-rudex，react-router
 2.webpack4.0
+
+# 介绍
+微服务系统的其中一个子项目例子
---
+++ src/view/routes/Error.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Error extends Component {
+  render() {
+    return (
+      <div>
+        没有这个页面
+      </div>
+    )
+  }
+}
